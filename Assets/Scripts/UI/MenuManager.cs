520cc30720e94ff51723ce05a67d4640ca48c2b9
irfanaldin24 <irfan.aldin50@gmail.com>
2020-04-15 17:49:15 +0700

    fix bug masalah main menu dan penambahan scene prolog
    udah aku tambahin prolog scene dan main menu bisa jalan untuk masalah pengaturan perpindahan scene dari prolog aku kasih script di prologmanager sedangkan di main menunya aku kasih script di mainmenumanager . terus pas terakhir di bagian 3 di scene prolog manager terdapat next yang terakhir aku belum connectin ke scene level in gamenya .

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject Options;
+    public GameObject Tampilan;
+
     public void Level1()
     {
         SceneManager.LoadScene("Prolog");
     }
+    public void Keluar()
+    {
+        Application.Quit();
+    }
+    public void optionsOn()
+    {
+        Options.SetActive(true);
+        Tampilan.SetActive(false);
+    }
+    public void optionsOff()
+    {
+        Options.SetActive(false);
+        Tampilan.SetActive(true);
+    }
+
 }
