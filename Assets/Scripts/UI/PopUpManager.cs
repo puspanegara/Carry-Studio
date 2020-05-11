e8a1c27de78d210f91383c901eed0a1baff13518
irfanaldin24 <irfan.aldin50@gmail.com>
2020-05-11 16:30:54 +0700

    Penambahan Animasi Karakter dan Update Prolog
    Sudah dibuatkan animasi controllernya serta animasi Pop up lalu Update Animasi Prolog

---
+++ Assets/Scripts/UI/PopUpManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpManager : MonoBehaviour
+{
+    public Animator anim;
+
+    public void PopMisi1()
+    {
+        anim.SetBool("Misi1", true);
+    }
+}
--- Assets/Scripts/UI/PrologManager.cs
+++ Assets/Scripts/UI/PrologManager.cs
@@ -6,34 +6,8 @@ using UnityEngine.SceneManagement;
 public class PrologManager : MonoBehaviour
     
 {
-    public GameObject Textsatu;
-    public GameObject Textdua;
-    public GameObject Texttiga;
-    public GameObject prolog1;
-    public GameObject prolog2;
-    public GameObject prolog3;
+    public void cussNextScene(){
 
-    void Start()
-    {
-       
-    }
-
-    public void dua()
-    {
-        if (Textsatu != null)
-        {
-            prolog2.SetActive(true);
-            prolog1.SetActive(false);
-            prolog3.SetActive(false);
-
-            Debug.Log("yaaa");
-        }
-    }
-    public void tiga()
-    {
-        prolog2.SetActive(false);
-        prolog1.SetActive(false);
-        prolog3.SetActive(true);
-        Debug.Log("yaaa");
+        SceneManager.LoadScene(4);
     }
 }
