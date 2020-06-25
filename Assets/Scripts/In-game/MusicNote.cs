9d8b2bba54846a797185d002fd9ff6607d99cd6f
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-06-25 09:42:23 +0700

    Membuat script untuk note Piano

---
+++ Assets/Scripts/In-game/MusicNote.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicNote : MonoBehaviour
+{
+    public AudioSource noteC;
+    public AudioSource noteD;
+    public AudioSource noteE;
+    public AudioSource noteF;
+    public AudioSource noteG;
+    public AudioSource noteA;
+    public AudioSource noteB;
+    public AudioSource noteC1;
+
+    public void CPlay()
+    {
+        noteC.Play();
+    }
+    public void DPlay()
+    {
+        noteD.Play();
+    }
+    public void EPlay()
+    {
+        noteE.Play();
+    }
+    public void FPlay()
+    {
+        noteF.Play();
+    }
+    public void GPlay()
+    {
+        noteG.Play();
+    }
+    public void APlay()
+    {
+        noteA.Play();
+    }
+    public void BPlay()
+    {
+        noteB.Play();
+    }
+    public void C1Play()
+    {
+        noteC1.Play();
+    }
+}
